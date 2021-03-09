599a6bdaf09b99a7c3010f92411685d4f2e3ee19
Anthony Ndegwa <anthon.n24@gmail.com>
2021-03-09 18:28:46 -0500

    update

--- script.js
+++ script.js
@@ -38,8 +38,64 @@ alert('Password copied');
 
 generateEL.addEventListener('click',() =>  {
 
+    const length = +lengthEL.value;
+    const hasLower =lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
 
-
-
-
+    resultEl.innerText = generatePassword(hasLower,hasUpper,hasSymbol,hasNumber,length);
 })
+
+
+function generatePassword(lower, upper, number, symbol, length) {
+
+let generatePassword = '';
+
+const typesCount = lower + upper + number + symbol;
+;
+const typesArr = [{lower},{upper},{symbol},{number}].filter(item => 
+    Object.values(item)[0]);
+
+
+    if(typesCount === 0) {
+
+        return '';
+    }
+//loop//
+for ( let i=0; i<length; i+typesCount) {
+
+typesArr.forEach(type => {  const funcName = Object.keys(type)[0];
+    generatePassword += randomFunc[funcName]();
+
+
+   });
+ 
+}
+ 
+
+
+ const finalPassword = generatePassword.slice(0, length);
+
+      return finalPassword; 
+
+}
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random()* 26) + 97);
+}
+
+function getRandomUpper() {
+
+    return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+}
+
+function getRandomSymbol() {
+    const symbol = '!@#$%^&*(){}[]=<>/,.'
+    return symbol = [Math.floor(Math.random()*symbol.length)];
+}
+
+function getRandomNumber() {
+
+return +string.fromCharCode*(Math.floor(Math.random()*10)+ 48);
+}
