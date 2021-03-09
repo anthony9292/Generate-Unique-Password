cea05d0dc7c3d3284c5c4d9d0aacd45da28343f6
Anthony Ndegwa <anthon.n24@gmail.com>
2021-03-09 17:34:10 -0500

    update

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-var charset= "01234567889abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ!@#$%^&*()_+=?<>:{[";
-var passwordLength= 16;
-var password:"";
-for(var i=0; i<passwordLength;i++) {
-   var randomnum = Math.floor(math.random()*charset.length);
-   password += charset.substring(randomnum, randomnum+1);
+const clipboard = document.getElementById('clipboard');
+const resultEl = document.getElementById('result');
+const lengthEL = document.getElementById('length');
+const uppercaseEl = document.getElementById ('uppercase');
+const lowercaseEl = document.getElementById ('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEL =  document.getElementById('generate')
 
 
+const randomFunc = {
+
+lower: getRandomLower,
+upper: getRandomUpper,
+number: getRandomNumber,
+symbol: getRandomSymbol
 }
-     
+
+/*code that copies password to clipboard/allowing u to save it in a secure location*/
+clipboard.addEventListener('click', () => {
+
+const textarea = document.createElement('textarea');
+const password = resultEl.innerText;
+
+if(!password) {return; }
+
+textarea.value = password;
+document.body.appendChild(textarea);
+textarea.select();
+document.execCommand('copy');
+textarea.remove();
+alert('Password copied');
+
+
+});
+
+
+
+generateEL.addEventListener('click',() =>  {
+
+
+
+
+
+})
