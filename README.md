2dcf05ce1a89a66c20e364ad7b79d9accd757c78
Anthony Ndegwa <anthon.n24@gmail.com>
2021-03-10 20:47:25 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Your-password
-password genarator
+
+<p> With this project i was stumbling to figure out the layout for it. i was confused at the beginning with how i how i was going to build it. i ran threw multiple designs as u can see from my github and decided to mix and match at the end.</p>
+
+****challenges I faced***
+
+1. Page layout and how i wanted to present the password generator 
+2. Windows alert method would not work smoothy on chrome so had to find other method
+   ----problem arose when i was playing around with alert method,  chrome would freeze up on input of numbers and symbols.
+3. Had Challenges trying to display the generated password in java
+4. Had Challenges trying to figure out the copy to clipboard function in java 
+5. Had challenges trying to work out how to add on click events(used google)
+6. Had challenges trying to figure out what was best to use getElementById or querySelector for this project.
+7. Was getting confused with the Math(Math.floor(Math.radom()))part of the code in java so used google to find a simpler way to code it.(was getting lost in the code).
+
+
+***screenshots of my code are in images***
+
+
+
+
+***link to my project*****
+
+
+
+
+
+
+Overall i had fun with this project.  
+
+
+
--- java/script.js
+++ java/script.js
@@ -1,3 +1,5 @@
+
+//DOM elements
 var _length = document.querySelector('input[name="length"]');
 var _lowercase = document.querySelector('input[name="lowercase"]');
 var _uppercase = document.querySelector('input[name="uppercase"]');
@@ -5,6 +7,8 @@ var _number = document.querySelector('input[name="number"]');
 var _symbol = document.querySelector('input[name="symbol"]');
 var copy = document.getElementById("copy");
 var generateButton = document.querySelector('.box button');
+
+
 
 
 const key_strings = {
@@ -16,7 +20,7 @@ const key_strings = {
 };
 
 
-  
+//generate password click event 
 generateButton.addEventListener("click", () => {
   var length = +_length.value;
   var activeLower = _lowercase.checked;
@@ -28,7 +32,7 @@ generateButton.addEventListener("click", () => {
  
  
  });
-
+//copy script to clipboard and alert  window alert box//
 copy.addEventListener("click", () => {
 	var _password = document.querySelector('input[type="text"]');
     if(_password.value != "" && _password.value != "Include any key string and define the length!"){
@@ -38,7 +42,7 @@ copy.addEventListener("click", () => {
 	}
 });
 
-
+//Generate password function 
 function generateRandomPassword(lower, upper, num, sym, length) {
   let MAIN_STRING = "";
 	let PASSWORD = "";
@@ -52,9 +56,6 @@ function generateRandomPassword(lower, upper, num, sym, length) {
 
 };
 
-
-
-
 for(i=0; i<Object.keys(options).length;i++){
    MAIN_STRING +=(Object.values(options)[i]) ? key_strings[Object.keys(options)[i]] : "";
 }
@@ -64,13 +65,12 @@ if(MAIN_STRING != "" && length > 0) {
   PASSWORD += MAIN_STRING[Math.floor(Math.random() * MAIN_STRING.length)];
   }
 
+//display generated password in text element 
   document.querySelector('input[type="text"]').value = PASSWORD;
-
-
-
 
 }else{
 
+//text if no click events are attacked
  document.querySelector('input[type="text"]').value = "Include any key string and define the length!";
 }
 
